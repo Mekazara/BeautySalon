831526e6daccdd48c41be50a37b5a155a15321db
mekazara <mekuchazara@gmail.com>
2021-01-15 11:16:08 +0600

    OrderViewset and Loginviewset created. Toke we need to not signup twice

--- candy_shop/service/views.py
+++ candy_shop/service/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from service.models import Master, Certificate, Service, Feedback
+from service.models import Master, Certificate, Service, Feedback, Order
 from service.serializers import MasterSerializer, CertificateSerializer, ServiceSerializer, FeedbackSerializer, \
-    MasterCertificateList
+    OrderSerializer, UserCreationSerializer
 
 
 class MasterViewset(viewsets.ModelViewSet):
@@ -21,3 +24,27 @@ class ServiceViewset(viewsets.ModelViewSet):
 class FeedbackViewset(viewsets.ModelViewSet):
     queryset = Feedback.objects.all()
     serializer_class = FeedbackSerializer
+
+class OrderViewset(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+class SignUp(APIView):
+    def post(self, request, *args, **kwargs):
+        serializer = UserCreationSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            user = serializer.save()
+            user.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class LoginView(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key})
+
+
