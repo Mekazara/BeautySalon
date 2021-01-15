774163002ce4c8f22606ab34c5dd1744f49b7298
mekazara <mekuchazara@gmail.com>
2021-01-15 11:15:22 +0600

    urls for Order, SignUp and Login

--- candy_shop/service/urls.py
+++ candy_shop/service/urls.py
@@ -1,14 +1,17 @@
-
 from django.urls import path, include
 from rest_framework import routers
-
-from service.views import MasterViewset, CertificateViewset, ServiceViewset, FeedbackViewset
+from service.views import MasterViewset, CertificateViewset, ServiceViewset, FeedbackViewset, OrderViewset, SignUp, \
+    LoginView
 
 router = routers.DefaultRouter()
 router.register('master', MasterViewset, basename='master')
 router.register('certificate', CertificateViewset, basename='certificate')
 router.register('service', ServiceViewset, basename='service')
 router.register('feedback', FeedbackViewset, basename='feedback')
+router.register('order', OrderViewset, basename='order')
+
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('register/', SignUp.as_view()),
+    path('login/', LoginView.as_view()),
 ]
